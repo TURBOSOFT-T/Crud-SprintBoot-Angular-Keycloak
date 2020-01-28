c7057a8624d04f69261b511d472deee6332f0380
TuemoT <thomastuemo@yahoo.fr>
2020-01-28 16:18:14 +0100

    Premier commit

---
+++ src/main/java/com/ipsas/gi/CrudSprintBootAngularKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.ipsas.gi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSprintBootAngularKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSprintBootAngularKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipsas/gi/CrudSprintBootAngularKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipsas.gi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSprintBootAngularKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
