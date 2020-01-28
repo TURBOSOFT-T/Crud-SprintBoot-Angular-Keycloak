15e1f86519e459bc4981ab4f314a4284c68e2f75
TuemoT <thomastuemo@yahoo.fr>
2020-01-28 18:33:40 +0100

    updated

--- src/main/java/com/ipsas/gi/entities/Product.java
+++ src/main/java/com/ipsas/gi/entities/Product.java
@@ -1,5 +1,21 @@
 package com.ipsas.gi.entities;
 
+import javax.annotation.Generated;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+
+
+
+@Entity
+@Table(value = "products")
 public class Product {
+	
+	private int code;
+	private String name;
+	private int price;
+	
 
 }
